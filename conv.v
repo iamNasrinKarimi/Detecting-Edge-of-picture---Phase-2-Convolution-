97b0234768cf2dac4de539579cd4e4b34bf75503
Nasrin Karimi <116264281+iamNasrinKarimi@users.noreply.github.com>
2023-08-25 19:08:28 +0330

    Add files via upload

---
+++ conv.v
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:02:36 01/06/2021 
+// Design Name: 
+// Module Name:    conv 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module conv( input clk, input data_in, input [1:0]row_in, input [1:0]col_in,
+input signed [7:0]data, output reg signed [7:0]out );
+reg [7:0]x[2:0][2:0];
+reg signed [3:0]kernel[2:0][2:0]; 
+integer m,n,sum;
+initial begin
+     kernel[0][0]= 1;
+	  kernel[0][1]= 2;
+	  kernel[0][2]= 1;
+	  kernel[1][0]= 0;
+	  kernel[1][1]= 0;
+	  kernel[1][2]= 0;
+	  kernel[2][0]= -1;
+	  kernel[2][1]= -2;
+	  kernel[2][2]= -1;
+end
+always @(posedge clk) begin
+if (data_in == 1) begin
+x[row_in][col_in] = data;
+end
+else if (data_in == 0) begin
+sum = 0;
+for (m = 0 ; m <= 2 ; m = m + 1) begin
+for (n = 0 ; n <= 2 ; n = n + 1) begin 
+sum = sum + x[m][n]*kernel[2-m][2-n];
+end
+end
+out = sum;
+end
+if (sum > 255 ) begin
+out = 255 ;
+end
+if (sum < 0 ) begin
+out = 0 ;
+end 
+end
+endmodule 
---
+++ convtest.v
@@ -0,0 +1,142 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   18:08:13 01/06/2021
+// Design Name:   conv
+// Module Name:   C:/.Xilinx/convolution/convtest.v
+// Project Name:  convolution
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: conv
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module convtest;
+
+	// Inputs
+	reg clk;
+	reg data_in;
+	reg [1:0] row_in;
+	reg [1:0] col_in;
+	reg [7:0] data;
+
+	// Outputs
+	wire [7:0] out;
+
+	// Instantiate the Unit Under Test (UUT)
+	conv uut (
+		.clk(clk), 
+		.data_in(data_in), 
+		.row_in(row_in), 
+		.col_in(col_in), 
+		.data(data), 
+		.out(out)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		data_in=0;
+		row_in=0;
+		col_in=0;
+		data=0;
+		
+#100;
+		clk = 1;
+		data_in = 1;
+		row_in = 0;
+		col_in = 0;
+		data = 1;
+#10;
+		clk = 0;
+#10;
+		clk = 1;
+		data_in = 1;
+		row_in = 0;
+		col_in = 1;
+		data = 2;
+#10;
+		clk = 0;
+#10;
+		clk = 1;
+		data_in = 1;
+		row_in = 0;
+		col_in = 2;
+		data = 3;
+#10;
+		clk = 0;
+
+#10;
+		clk = 1;
+		data_in = 1;
+		row_in = 1;
+		col_in = 0;
+		data = 4;		
+#10;
+		clk = 0;
+
+#10;
+
+		clk = 1;
+		data_in = 1;
+		row_in = 1;
+		col_in = 1;
+		data = 5;
+#10;
+		clk = 0;
+
+#10;
+		clk = 1;
+		data_in = 1;
+		row_in = 1;
+		col_in = 2;
+		data = 6;		
+#10;
+		clk = 0;
+#10;
+		clk = 1;
+		data_in = 1;
+		row_in = 2;
+		col_in = 0;
+		data = 7;
+#10;
+		clk = 0;
+
+#10;
+		clk = 1;
+		data_in = 1;
+		row_in = 2;
+		col_in = 1;
+		data = 8;		
+#10;
+		clk = 0;
+#10;
+		clk = 1;
+		data_in = 1;
+		row_in = 2;
+		col_in = 2;
+		data = 9;	
+#10;
+      clk = 0;
+#10;
+      clk = 1;
+		data_in = 0;
+		row_in = 0;
+		col_in = 0;	
+		data=0;
+#10;
+clk=0;
+
+end
+endmodule 
